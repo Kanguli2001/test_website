9b336488a70d1f9de518e271bf8d232b3b3cd91f
Kanguli2001 <williamkanguli3@gmail.com>
2025-11-27 15:20:01 +0300

    our first laravel app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">
+                        Welcome to Chirper!
+                    </h1>
+
+                    <p class="mt-4 text-base-content/60">
+                        Welcome to Chirper, a simple social media platform where you can share your thoughts and connect with others. Sign up today to start chirping!
+                    </p>
+                </div>
+            </div>
+        </div>    
+    </div>
+</x-layout>
